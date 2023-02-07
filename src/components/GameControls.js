d998b228e3a1fe0dff5c4d88c9edf3c281012437
Onneqq <adampenno93@gmail.com>
2023-02-07 13:08:20 +1100

    added scale component in gamecontrols

--- src/components/GameControls.js
+++ src/components/GameControls.js
@@ -1,9 +1,24 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+
+import React, { useState } from 'react'
+import { Slider } from '@mui/material'
 import './CSS/GameControls.scss'
 
 function GameControls (props) {
+  const [value, setValue] = useState(1900)
+
   return (
-        <p>something</p>
+
+    <Slider
+  aria-label="Small steps"
+  defaultValue={0.00000005}
+  getAriaValueText={(e) => console.log(e)}
+  step={1}
+  marks
+  min={1900}
+  max={2023}
+  valueLabelDisplay="auto"
+/>
   )
 }
 
